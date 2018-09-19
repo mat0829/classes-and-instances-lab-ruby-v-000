ea45fd0e16737bf342e36d0921151d5a4f3aa5ca
David Anderson <matandersonlv@hotmail.com>
2018-09-19 18:14:14 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+# Creating a new class called "Dog"
+class Dog
+
+end
+
+# Creating 3 new instances of the Dog class and assigning them to local variables
+fido = Dog.new
+fido
+
+snoopy = Dog.new
+snoopy
+
+lassie = Dog.new
+lassie
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+# Creating a new class called "Person"
+class Person
+
+end
+
+# Creating 2 new instances of the Person class and assigning them local variables
+adele_goldberg = Person.new
+adele_goldberg
+
+alan_kay = Person.new
+alan_kay
